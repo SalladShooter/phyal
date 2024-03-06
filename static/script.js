71d44212d122b8e1604585b45201ff1d01485b54
SalladShooter <139503084+SalladShooter@users.noreply.github.com>
2024-03-06 12:20:50 +0000

    Fixed script.js

--- static/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    document.querySelector('.span').innerText = 'Button Clicked!';
-    console.log("Button Clicked");
-});
